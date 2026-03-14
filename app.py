81d8adce61417f24b59c90124db18b46f7d243de
irize104-bit <irize104@gmail.com>
2026-03-14 17:36:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -199,10 +199,8 @@ def download_monthly_excel():
         as_attachment=True
     )
 if __name__ == "__main__":
-
-    with app.app_context():
-    db.create_all()
-
-if __name__ == "__main__":
+  with app.app_context():
+     db.create_all()
+      
     app.run(debug=True)
 
